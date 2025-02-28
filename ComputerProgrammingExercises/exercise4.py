d05dd083dc1e101edc8b6ba8cc7f9fcc6e20c5a5
Artur Zacniewski <a.zacniewski@gmail.com>
2025-02-28 09:32:25 +0100

    Add exercise4

---
+++ ComputerProgrammingExercises/exercise4.py
@@ -0,0 +1,8 @@
+my_number = input("Please, type your number: ")
+print(type(my_number))
+
+my_number_changed_to_int = int(my_number)
+print(type(my_number_changed_to_int))
+
+squared_value = my_number_changed_to_int ** 2
+print("Squared value of input number is: ", squared_value)
