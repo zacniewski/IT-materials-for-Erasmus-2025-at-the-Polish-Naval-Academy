508507eb58f6eea0852d872f6df0da97add80e36
Artur Zacniewski <a.zacniewski@gmail.com>
2025-02-28 08:27:11 +0100

    Add Python exercises

---
+++ ComputerProgrammingExercises/exercise1.py
@@ -0,0 +1,7 @@
+def some_super_function():
+    print("Hello")
+    print('Hello again')
+    a = 6
+    print(a)
+
+some_super_function()
---
+++ ComputerProgrammingExercises/exercise2.py
@@ -0,0 +1,25 @@
+def add_two_numbers(number1, number2):
+    return number1 + number2
+
+
+def print_text(some_text):
+    print(some_text)
+
+
+def repeat(word, n):
+    print(word * n)
+
+
+def multiply_abs_value(num, number_of_repetions):
+    print(abs(num) * number_of_repetions)
+
+multiply_abs_value(-12, 5)
+
+
+
+
+
+# repeat("Yooooo ", 5)
+# repeat("ABCDEF ", 10)
+# result = add_two_numbers(10, 40)
+# print(result)
